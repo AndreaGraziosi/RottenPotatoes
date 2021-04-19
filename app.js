729e872dd14733a9ea6bc8e797f68330bcaa4d9b
Andrea_Graziosi <andreaggraziosi@gmail.com>
2021-04-18 20:53:39 -0700

    create an root route express and handlebars set up

--- app.js
+++ app.js
@@ -11,11 +11,26 @@ app.set('view engine', 'handlebars');
 
 
 
-app.get('/', (req, res) => {
-  // res.send('Hello World!')
-  res.render('home', { msg: 'Handlebars are Cool!' });
+// app.get('/', (req, res) => {
+//   // res.send('Hello World!')
+//   res.render('home', { msg: 'Handlebars are Cool!' });
 
+// })
+// app.js
+
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+  { title: "Great Review", movieTitle: "Batman II" },
+  { title: "Awesome Movie", movieTitle: "Titanic" }
+]
+
+// INDEX
+app.get('/', (req, res) => {
+  res.render('reviews-index', { reviews: reviews });
 })
+
+
+
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
