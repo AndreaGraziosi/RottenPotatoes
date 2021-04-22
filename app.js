ec0c551f4683a6ea95a8ecf1c3966416164b64d4
Andrea_Graziosi <andreaggraziosi@gmail.com>
2021-04-22 15:37:37 -0700

    Users can edit and update results

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+
 const express = require('express')
+
+const methodOverride = require('method-override')
 const app = express()
 //models
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
 
+// override with POST having ?_method=DELETE or ?_method=PUT
+app.use(methodOverride('_method'))
 
 
 // The following line must appear AFTER const app = express() and before your routes!
@@ -57,13 +62,13 @@ app.get('/', (req, res) => {
 
 // NEW
 app.get('/reviews/new', (req, res) => {
-  res.render('reviews-new', {});
+  res.render('reviews-new', {title: "New Review"});
 })
 
-app.post('/reviews', (req, res) => {
-  console.log(req.body);
+// app.post('/reviews', (req, res) => {
+//   console.log(req.body);
   // res.render('reviews-new', {});
-})
+// })
 // CREATE
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
@@ -82,9 +87,27 @@ app.get('/reviews/:id', (req, res) => {
   }).catch((err) => {
     console.log(err.message);
   })
-});
+})
+
+// EDIT
+app.get('/reviews/:id/edit', (req, res) => {
+  Review.findById(req.params.id, function(err, review) {
+    res.render('reviews-edit', {review: review, title: "Edit Review"});
+  })
+})
+  
 
 
+// UPDATE
+app.put('/reviews/:id', (req, res) => {
+  Review.findByIdAndUpdate(req.params.id, req.body)
+    .then(review => {
+      res.redirect(`/reviews/${review._id}`)
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
 
 
 app.listen(3000, () => {
